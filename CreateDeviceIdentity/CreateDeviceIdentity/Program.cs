9bd5f7d481f11d66dfc5ef10c2d213bea76c7021
mirkub <mkubovcik@hotmail.com>
2015-12-16 12:19:32 +0100

    baseline

---
+++ CreateDeviceIdentity/CreateDeviceIdentity/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+
+
+namespace CreateDeviceIdentity
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        static string connectionString = "HostName=SantaGLM-IoTHub.azure-devices.net;SharedAccessKeyName=registryReadWrite;SharedAccessKey=+KG5ihUM8LuE3MStlQF1CCCzO7p05YwvkVPYTwiMyzo=";
+
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+        }
+
+        private async static Task AddDeviceAsync()
+        {
+            string deviceId = "santasFirstDevice";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+        }
+    }
+}
